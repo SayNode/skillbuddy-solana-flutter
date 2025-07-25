0c6b87f1ad08961ebf8a54eaa6c681dd61c1b4b6
ruby1213 <121198995+ruby1213@users.noreply.github.com>
2025-07-25 11:46:45 +0200

    Add Solana course distinction in CourseCard and include Satoshi icon

--- lib/page/explore_course/discover_courses_page.dart
+++ lib/page/explore_course/discover_courses_page.dart
@@ -392,6 +392,9 @@ class DiscoverCoursesPage extends GetView<DiscoverCoursesController> {
                                                     i < entry.value.length;
                                                     i++) ...<Widget>[
                                                   CourseCard(
+                                                    isSolanaCourse:
+                                                        entry.key.title ==
+                                                            'Solana',
                                                     course: entry.value[i],
                                                     onTap: () => controller
                                                         .goToCourseDetails(
